510097334b7eb6a659639295264446aa24a810f2
gyarudos <k64.yuta29@gmail.com>
2025-07-09 22:01:09 +0900

    データクラスとファクトリメソッドの作成

--- src/main/kotlin/com/example/demo/MessageController.kt
+++ src/main/kotlin/com/example/demo/MessageController.kt
@@ -2,11 +2,16 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/")
 class MessageController {
-    @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    @GetMapping
+    fun listMessages() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
 }
