fef05cf64a44b531966386666d6a6400be0000aa
gyarudos <k64.yuta29@gmail.com>
2025-07-09 23:13:45 +0900

    データクラスとファクトリメソッドの作成

---
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -0,0 +1,27 @@
+// MessageService.kt
+package com.example.demo
+
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
+import java.util.*
+
+@Service
+class MessageService(private val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }.singleOrNull()
+
+    fun save(message: Message): Message {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values ( ?, ? )",
+            id, message.text
+        )
+        return message.copy(id = id)
+    }
+}
